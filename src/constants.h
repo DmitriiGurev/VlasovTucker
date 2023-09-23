17f25ffb37b51e3d7c7f5debab54c5c3c9ffa50a
D Guryev <mixmaster123@mail.ru>
2023-09-23 20:16:09 +0300

    Update constants

--- src/constants.h
+++ src/constants.h
@@ -7,4 +7,7 @@ const double elMass       = 9.1e-31;   // kg
 const double elCharge     = 1.6e-19;   // C
 const double epsilon0     = 8.85e-12;  // F m^-1
 const double electronvolt = 11604.518; // K
-const double boltzConst   = 1.38e-23;  // m^2 kg s^-2 K^-1
+// const double boltzConst   = 1.38e-23;  // m^2 kg s^-2 K^-1
+
+// Temporary
+const double boltzConst = 1;

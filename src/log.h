dee6c200f51bb2678073b00d139601bed6775123
D Guryev <mixmaster123@mail.ru>
2023-09-23 20:15:49 +0300

    Add a logger class

---
+++ src/log.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+enum class LogLevel
+{
+    None,
+    Console,
+    TextFile,
+    AllText
+};
+
+/// TODO: Remove definitions from the class 
+class Log
+{
+public:
+    Log() : _level(LogLevel::None), _fPrefix("") {}
+
+    Log(LogLevel level, std::string prefix = "") : 
+        _level(level), _fPrefix(prefix)
+    {
+        if (_PrintToFile())
+            _fOut.open(_fPrefix + "log.txt");
+    }
+
+    Log& operator=(const Log& other)
+    {
+        _fOut.close();
+        
+        _level = other._level;
+        _fPrefix = other._fPrefix;
+        
+        if (_PrintToFile())
+            _fOut.open(_fPrefix + "log.txt");
+
+        return *this;
+    }
+
+    ~Log()
+    {
+        if (_PrintToFile())
+            _fOut.close();
+    }
+
+    template<typename T>
+    Log& operator<<(const T& data)
+    {
+        if (_PrintToConsole())
+            std::cout << data;
+
+        if (_PrintToFile())
+        {
+            assert(_fOut.is_open());
+            _fOut << data << std::flush;
+        }
+        
+        return *this;
+    }
+
+private:
+    bool _PrintToFile() const
+    {
+        return _level == LogLevel::AllText || _level == LogLevel::TextFile;
+    }
+
+    bool _PrintToConsole() const
+    {
+        return _level == LogLevel::AllText || _level == LogLevel::Console;
+    }
+
+private:
+    LogLevel _level;
+
+    std::ofstream _fOut; 
+    std::string _fPrefix;
+};

88f8be3a8f2835d3e91c6bc4de097e4ad46507b1
D Guryev <mixmaster123@mail.ru>
2024-02-02 21:16:45 +0300

    Update run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -21,41 +21,30 @@ int main(int argc, char *argv[])
     Mesh mesh(meshFileName);
     timer.PrintSectionTime("Mesh initialization");
 
-    cout << mesh.faces.size() << " faces, ";
-    cout << mesh.tets.size() << " tets\n";
+    cout << mesh.faces.size() << " faces, " << mesh.tets.size() << " tets\n";
 
-    int nCells = 41;
-    double minVX = -7;
-    double maxVX = 7;
-    VelocityGrid<Tensor> vGrid({ nCells, 3,    3   },
-                               { maxVX,  0.1,  0.1 },
-                               { minVX, -0.1, -0.1 });
+    // VelocityGrid<Tensor> vGrid({21, 9, 9}, {-2, -0.1, -0.1}, {2, 0.1, 0.1});
+    VelocityGrid<Tensor> vGrid({13, 11, 9}, {-2, -2, -2}, {2, 2, 2});
 
     PlasmaParameters plasmaParams(&mesh, &vGrid);
     plasmaParams.species = ParticleType::Custom;
-    plasmaParams.mass    = 1;
-    plasmaParams.charge  = 10;
+    plasmaParams.mass = 1;
+    plasmaParams.charge = 10;
 
     PlasmaParameters::MaxwellPDF paramsPDF;
 
-    auto rhoFunc = [](const Point& p)
-    {
-        return 10 + 0.2 * sin(1 * p.coords[0] * (2 * pi));
-    };
+    auto rhoFunc = [](const Point& p) { return 10 + 0.2 * sin(1 * p.coords[0] * (2 * pi)); };
 
     paramsPDF.physDensity = move(ScalarField(&mesh, rhoFunc));
     paramsPDF.temperature = 0;
-    paramsPDF.mostProbableV = { 0, 0, 0 };
+    paramsPDF.mostProbableV = {1, 0, 0};
 
-    cout << "The PDF takes up " << vGrid.nCellsTotal * mesh.tets.size() * 8 / pow(10, 6) <<
-        " MB of RAM\n"; 
-        
     plasmaParams.SetPDF<PlasmaParameters::MaxwellPDF>(paramsPDF);
 
     VTK::WriteDistribution("initial_distribution", vGrid, plasmaParams.pdf[mesh.tets.size() / 2]);
     VTK::WriteCellScalarData("initial_density", mesh, plasmaParams.Density());
 
     Solver solver(&mesh, &vGrid, &plasmaParams);
-    // solver.writeStep = 200;
+    solver.writeStep = 100;
     solver.Solve(100000);
 }

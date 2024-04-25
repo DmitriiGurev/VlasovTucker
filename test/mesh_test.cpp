6d553ab116082f63c6be40e70b3a932b34a5c80f
D Guryev <mixmaster123@mail.ru>
2024-04-25 13:30:06 +0300

    Update tests

--- test/mesh_test.cpp
+++ test/mesh_test.cpp
@@ -11,4 +11,10 @@ int main(int argc, char *argv[])
     Mesh mesh(argv[1]);
     mesh.Reconstruct();
     WriteMeshVTK("mesh", mesh);
+    
+    cout << mesh.points.size() << " points\n";
+    cout << mesh.faces.size() << " faces\n";
+    cout << mesh.tets.size() << " tetrahedra\n";
+
+    cout << "Element size: " << mesh.AverageCellSize() << "\n";
 }
